b4fb23fba2b364e75500cde0c43759582289a790
Curtis Stone <curtisjstone1@gmail.com>
2020-10-03 19:33:55 -0400

    Setup formating for amount and createdAt w/ moment.js and numeral.js

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ description, amount, createdAt, id }) => ( // already not connected to store so default can be used in testing
   // destructuring used to seperate the properties passed from props.expenses in
@@ -10,8 +12,9 @@ const ExpenseListItem = ({ description, amount, createdAt, id }) => ( // already
       <h3>{description}</h3>
     </Link>
     <p>
-      {" "}
-      {amount} - {createdAt}
+      {numeral(amount / 100).format('$0,0.00')} 
+      - 
+      {moment(createdAt).format('MMMM Do, YYYY')}
     </p>
   </div>
 );
